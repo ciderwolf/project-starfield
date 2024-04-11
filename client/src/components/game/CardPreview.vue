bef788aeff9ff93bd98940cf18a76a4217bd9856
bsiderowf <bsiderowf@gmail.com>
2024-04-10 20:38:32 -0700

    View rotated card preview

--- client/src/components/game/CardPreview.vue
+++ client/src/components/game/CardPreview.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
 import { useBoardStore, type OracleId } from '@/stores/board';
-import { ref } from 'vue';
+import { computed, onMounted, onUnmounted, ref } from 'vue';
 
 defineExpose({ showPreview, hidePreview });
 
 const visible = ref(false);
 const cardId = ref<OracleId | null>(null);
 const backFace = ref(false);
+
+const board = useBoardStore();
+const rotated = ref(false);
+const showOppositeFace = ref(false);
+const hasOtherFace = computed(() => {
+  if (cardId.value == null) {
+    return false;
+  }
+  return board.oracleInfo[cardId.value]?.backImage;
+})
 
 function showPreview(id: OracleId, backFaceValue: boolean) {
   visible.value = true;
@@ -22,9 +32,9 @@ function getCardUrl() {
   if (cardId.value === null) {
     return '';
   }
-  const board = useBoardStore();
-  const card = board.oracleInfo[cardId.value]
-  if (backFace.value && card.backImage) {
+  const card = board.oracleInfo[cardId.value];
+
+  if (backFace.value != showOppositeFace.value && card.backImage) {
     return card.backImage;
   } else {
     return card.image;
@@ -32,10 +42,40 @@ function getCardUrl() {
 
 }
 
+function keyPressed(e: KeyboardEvent) {
+  if (e.key === 'Alt') {
+    rotated.value = true;
+  } else if (e.key === 'Shift') {
+    showOppositeFace.value = true;
+  }
+}
+
+function keyReleased(e: KeyboardEvent) {
+  if (e.key === 'Alt') {
+    rotated.value = false;
+  } else if (e.key === 'Shift') {
+    showOppositeFace.value = false;
+  }
+}
+
+onMounted(() => {
+  window.addEventListener('keydown', keyPressed);
+  window.addEventListener('keyup', keyReleased);
+})
+
+onUnmounted(() => {
+  window.removeEventListener('keydown', keyPressed);
+  window.removeEventListener('keyup', keyReleased);
+})
+
 </script>
 
 <template>
-  <img v-if="visible" class="card-preview" :src="getCardUrl()">
+  <div class="card-preview" v-if="visible">
+    <img class="card-preview-image" :class="{ rotated }" :src="getCardUrl()">
+    <span class="preview-help-text" v-if="hasOtherFace">Hold shift to view other face</span>
+    <span class="preview-help-text">Hold Alt to rotate</span>
+  </div>
 </template>
 
 <style scoped>
@@ -43,9 +83,29 @@ function getCardUrl() {
   position: fixed;
   top: 25%;
   right: 0;
-  height: 50%;
   z-index: 100;
   pointer-events: none;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.card-preview-image {
+  height: 50%;
   border-radius: 10px;
+}
+
+.card-preview-image.rotated {
+  transform: rotate(90deg) translate(50%, 50%);
+  transform-origin: 100% 50%;
+}
+
+.preview-help-text {
+  margin: none;
+  padding: 0 10px;
+  color: white;
+  background: #0008;
+
 }
 </style>
