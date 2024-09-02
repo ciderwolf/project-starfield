440abbe82b77873728c6e0558d0bd5fd6bba888b
bsiderowf <bsiderowf@gmail.com>
2024-09-01 19:00:08 -0700

    Allow setting secure headers over http

--- server/src/main/kotlin/starfield/Application.kt
+++ server/src/main/kotlin/starfield/Application.kt
@@ -3,6 +3,7 @@ package starfield
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.*
 import io.ktor.server.routing.*
 import starfield.cli.Cli
 import starfield.data.dao.DatabaseSingleton
@@ -25,4 +26,14 @@ fun Application.module() {
     configureSockets()
     configureRouting()
     install(IgnoreTrailingSlash)
+
+    // allow https headers to be set on https
+    // (this is a hack)
+    intercept(ApplicationCallPipeline.Plugins) {
+        @Suppress( "DEPRECATION")
+        val endpoint = call.attributes.computeIfAbsent(MutableOriginConnectionPointKey) {
+            MutableOriginConnectionPoint(call.request.origin)
+        }
+        endpoint.scheme = "https"
+    }
 }
---
+++ server/src/main/kotlin/starfield/ConnectionPointPatch.kt
@@ -0,0 +1,40 @@
+package io.ktor.server.plugins
+
+import io.ktor.http.*
+import kotlin.reflect.KProperty
+
+private class AssignableWithDelegate<T : Any>(val property: () -> T) {
+    private var assigned: T? = null
+
+    operator fun getValue(thisRef: Any, property: KProperty<*>): T = assigned ?: property()
+
+    operator fun setValue(thisRef: Any, property: KProperty<*>, value: T) {
+        assigned = value
+    }
+}
+
+class MutableOriginConnectionPoint internal constructor (
+    delegate: RequestConnectionPoint
+) : RequestConnectionPoint {
+
+    override var version: String by AssignableWithDelegate { delegate.version }
+    override var uri: String by AssignableWithDelegate { delegate.uri }
+    override var method: HttpMethod by AssignableWithDelegate { delegate.method }
+    override var scheme: String by AssignableWithDelegate { delegate.scheme }
+
+    @Suppress("DEPRECATION")
+    @Deprecated("Use localHost or serverHost instead")
+    override var host: String by AssignableWithDelegate { delegate.host }
+    override var localHost: String by AssignableWithDelegate { delegate.localHost }
+    override var serverHost: String by AssignableWithDelegate { delegate.serverHost }
+    override var localAddress: String by AssignableWithDelegate { delegate.localAddress }
+
+    @Suppress("DEPRECATION")
+    @Deprecated("Use localPort or serverPort instead")
+    override var port: Int by AssignableWithDelegate { delegate.port }
+    override var localPort: Int by AssignableWithDelegate { delegate.localPort }
+    override var serverPort: Int by AssignableWithDelegate { delegate.serverPort }
+    override var remoteHost: String by AssignableWithDelegate { delegate.remoteHost }
+    override var remotePort: Int by AssignableWithDelegate { delegate.remotePort }
+    override var remoteAddress: String by AssignableWithDelegate { delegate.remoteAddress }
+}
